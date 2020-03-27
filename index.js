083028b74e27d77b8243c69a8c3424fc01482509
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-03-27 17:51:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function(happyHolidays) {
-  return 
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`
 }
